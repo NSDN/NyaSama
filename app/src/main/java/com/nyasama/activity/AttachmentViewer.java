cb157f5cf9aa244c17ce6726ff88f3d271b9ff36
oxyflour <a6905688@gmail.com>
2014-12-12 01:16:06 +0800

    update attachment order

--- app/src/main/java/com/nyasama/activity/AttachmentViewer.java
+++ app/src/main/java/com/nyasama/activity/AttachmentViewer.java
@@ -10,11 +10,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Response;
@@ -34,6 +35,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
@@ -41,9 +44,72 @@ public class AttachmentViewer extends FragmentActivity {
 
     private static String TAG = "ImageViewer";
 
+    private ViewPager mPager;
+    private TextView mTitle;
     private FragmentPagerAdapter mPageAdapter;
     private List<Attachment> mAttachmentList = new ArrayList<Attachment>();
     private Map<String, Bitmap> mBitmapCache = new HashMap<String, Bitmap>();
+
+    public void showAttachmentList() {
+        ListView listView = new ListView(this);
+        List<String> names = new ArrayList<String>();
+        for (Attachment attachment : mAttachmentList)
+            names.add(attachment.name);
+        listView.setAdapter(new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_activated_1,
+                android.R.id.text1,
+                names));
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Attachments")
+                .setView(listView)
+                .show();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mPager.setCurrentItem(i);
+                dialog.cancel();
+            }
+        });
+    }
+
+    public void updatePagerTitle(int position) {
+        mTitle.setVisibility(position >= 0 && mAttachmentList.size() > 0 ?
+                View.VISIBLE : View.GONE);
+        if (position >= 0) {
+            int len = mAttachmentList.size();
+            Attachment item = mAttachmentList.get(position);
+            String text = item.name;
+            if (len > 1)
+                text += " (" + (position+1) + "/" + mAttachmentList.size() + ")";
+            mTitle.setText(text);
+        }
+    }
+
+    static Pattern msgPathPattern = Pattern.compile("<img[^>]* file=\"(.*?)\"");
+    static Pattern msgMatcher = Pattern.compile("<ignore_js_op>(.*?)</ignore_js_op>",
+            Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
+    static List<Attachment> compileAttachments(String message, final List<Attachment> attachments) {
+        List<Attachment> newAttachments = new ArrayList<Attachment>();
+
+        Map<String, Attachment> map = new HashMap<String, Attachment>();
+        for (Attachment attachment : attachments) {
+            map.put(attachment.src, attachment);
+            if (!attachment.isImage)
+                newAttachments.add(attachment);
+        }
+
+        Matcher matcher = msgMatcher.matcher(message);
+        while (matcher.find()) {
+            Matcher pathMatcher = msgPathPattern.matcher(matcher.group(1));
+            if (pathMatcher.find()) {
+                Attachment attachment = map.get(pathMatcher.group(1));
+                if (attachment != null) newAttachments.add(attachment);
+            }
+        }
+
+        return newAttachments;
+    }
 
     public void loadAttachments() {
         Discuz.execute("viewthread", new HashMap<String, Object>() {{
@@ -53,6 +119,7 @@ public class AttachmentViewer extends FragmentActivity {
         }}, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject data) {
+                int position = -1;
                 if (data.has(Discuz.VOLLEY_ERROR)) {
                     Helper.toast(R.string.network_error_toast);
                 }
@@ -81,17 +148,19 @@ public class AttachmentViewer extends FragmentActivity {
                         JSONObject var = data.getJSONObject("Variables");
                         JSONArray postlist = var.getJSONArray("postlist");
                         mAttachmentList.clear();
-                        if (postlist.length() == 1) {
+                        if (postlist.length() >= 1) {
                             Post post = new Post(postlist.getJSONObject(0));
-                            mAttachmentList = post.attachments;
+                            mAttachmentList = compileAttachments(post.message, post.attachments);
                         }
                         mPageAdapter.notifyDataSetChanged();
+                        position = 0;
                     }
                     catch (JSONException e) {
                         Log.e(TAG, "JsonError: Load Post List Failed (" + e.getMessage() + ")");
                         Helper.toast(R.string.load_failed_toast);
                     }
                 }
+                updatePagerTitle(position);
             }
         });
     }
@@ -101,8 +170,24 @@ public class AttachmentViewer extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attachment_viewer);
 
-        ViewPager pager = (ViewPager) findViewById(R.id.view_pager);
-        pager.setAdapter(mPageAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+        mPager = (ViewPager) findViewById(R.id.view_pager);
+        mPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                updatePagerTitle(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+        mPager.setAdapter(mPageAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(final int i) {
                 return new Fragment() {
@@ -117,8 +202,7 @@ public class AttachmentViewer extends FragmentActivity {
                             if (bitmap != null) {
                                 imageView.setImageBitmap(bitmap);
                                 new PhotoViewAttacher(imageView);
-                            }
-                            else {
+                            } else {
                                 imageView.setImageResource(android.R.drawable.ic_menu_gallery);
                                 ImageRequest imageRequest = new ImageRequest(url, new Response.Listener<Bitmap>() {
                                     @Override
@@ -131,8 +215,7 @@ public class AttachmentViewer extends FragmentActivity {
                                 ThisApp.requestQueue.add(imageRequest);
                             }
                             return imageView;
-                        }
-                        else {
+                        } else {
                             View view = inflater.inflate(R.layout.fragment_attachment_item, container, false);
                             TextView textView = (TextView) view.findViewById(R.id.name);
                             textView.setText(attachment.name + " (" + attachment.size + ")");
@@ -147,30 +230,15 @@ public class AttachmentViewer extends FragmentActivity {
                 return mAttachmentList.size();
             }
         });
+        mTitle = (TextView) findViewById(R.id.view_title);
+        mTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAttachmentList();
+            }
+        });
 
         loadAttachments();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_attach_image_viewer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
