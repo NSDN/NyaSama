10e66ab80d6452cca1d2a56605b6ef5b411db693
krhkrh <krhkrh@users.noreply.github.com>
2014-12-28 12:19:23 -0500

    LruCache comment

--- app/src/main/java/com/nyasama/util/BitmapLruCache.java
+++ app/src/main/java/com/nyasama/util/BitmapLruCache.java
@@ -8,6 +8,19 @@ import com.android.volley.toolbox.ImageLoader;
 /**
  * Created by oxyflour on 2014/11/19.
  * REF: http://blog.lemberg.co.uk/volley-part-3-image-loader
+ * 
+ *
+ * 用来为ThisApp 中的 imageLoader 提供存储空间的 Cache
+ * 其实用起来和 Hashmap 没什么两样
+ * 
+ * getBitmap 和 putBitmap 是 ImageCache 中的方法
+ * getBitmap 是按URL 取出bitmap
+ * putBitmap 是当下载完成后，放入Cache
+ * 
+ * 其他是LruCache中的方法，LruCache 的意思是 使用 Last Recent Used 算法的 Cache
+ * 至于具体为什么默认要用 maxmemory/8 Kbytes 左右的空间，我也不明白
+ * 
+ * 
  */
 public class BitmapLruCache
         extends LruCache<String, Bitmap>
