56baf8d3fc0de5bef2c646b780f539ba19facb13
krhkrh <krhkrh@users.noreply.github.com>
2014-12-29 16:41:50 -0500

    common

--- app/src/main/java/com/nyasama/util/CommonListAdapter.java
+++ app/src/main/java/com/nyasama/util/CommonListAdapter.java
@@ -12,12 +12,16 @@ import java.util.List;
 /**
  * Created by Oxyflour on 2014/11/13.
  * REF: http://blog.csdn.net/lmj623565791/article/details/38902805
+ * 
+ * commonListAdapter 是为各种ListView 准备的适配器，也是一个通用工具
+ * 因此在这里更多的考虑的是通用性而非具体实现，用例应该有不少，比如DiscuzForumIndexFragment 中的用例
+ * 也可以把这个类做成接口啦
  */
 public abstract class CommonListAdapter<T> extends BaseAdapter {
 
     protected List<T> mList;
     protected int mLayout;
-
+//构造函数区
     public CommonListAdapter() {
     }
 
@@ -29,7 +33,9 @@ public abstract class CommonListAdapter<T> extends BaseAdapter {
         mList = list;
         mLayout = layout;
     }
+//构造函数区
 
+//基本（酱油）函数区
     @Override
     public int getCount() {
         return mList.size();
@@ -44,7 +50,17 @@ public abstract class CommonListAdapter<T> extends BaseAdapter {
     public long getItemId(int position) {
         return position;
     }
+//基本（酱油）函数区
 
+//重点函数区
+/*
+Adapter 的文档：http://developer.android.com/reference/android/widget/Adapter.html
+adapter 的工作原理是：
+自动创建用户能看见的View以及接下来可能会看见的View
+当adapter 创建View 时，就会自动调用getView 函数
+比较值得说的时第二个参数，convertView 是被用户移到视窗外的View
+因此可以拿来，装上新的数据重用
+*/
     @Override
     @SuppressWarnings("unchecked")
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -55,17 +71,20 @@ public abstract class CommonListAdapter<T> extends BaseAdapter {
         convertView((ViewHolder) convertView.getTag(), getItem(position));
         return convertView;
     }
-
+//这个有用过，应该用不着加 @SuppressWarnings("unused") 了
     @SuppressWarnings("unused")
     public View createView(ViewGroup parent, int position) {
         return LayoutInflater.from(parent.getContext())
                 .inflate(mLayout, parent, false);
     }
 
+//重写这个函数，能改变convertView中的内容
     public abstract void convertView(ViewHolder viewHolder, T item);
 
+//这个类一方面管理convertView中的组件，另一方面也是为了提高效率
     public class ViewHolder {
-
+//SparseArray 是比较推荐存储方式
+//文档见：https://developer.android.com/training/articles/memory.html 中的 Use optimized data containers 节
         private final SparseArray<View> mViews;
         private final View mConvertView;
 
@@ -77,7 +96,7 @@ public abstract class CommonListAdapter<T> extends BaseAdapter {
         public View getConvertView() {
             return mConvertView;
         }
-
+//findViewById 是比较浪费时间的操作，因此创建的时候放入Array，以后从Array 中找比较快
         public View getView(int viewId) {
             View view = mViews.get(viewId);
             if (view == null) {
