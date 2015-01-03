df3e15cbd5acb1a186d83dc3e9e35b9f62a0735a
oxyflour <a6905688@gmail.com>
2015-01-03 16:48:50 +0800

    remove title for AttachmentViewer

--- app/src/main/java/com/nyasama/activity/AttachmentViewer.java
+++ app/src/main/java/com/nyasama/activity/AttachmentViewer.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -232,6 +233,7 @@ public class AttachmentViewer extends BaseThemedActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_attachment_viewer);
 
         mPager = (ViewPager) findViewById(R.id.view_pager);
