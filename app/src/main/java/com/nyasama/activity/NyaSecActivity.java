784cfe27590a5695210b5174ab0f4a0d2eebc1f8
oxyflour <oxyflour@hotmail.com>
2016-01-25 18:44:19 +0800

    add kaomoji game

--- app/src/main/java/com/nyasama/activity/NyaSecActivity.java
+++ app/src/main/java/com/nyasama/activity/NyaSecActivity.java
@@ -2,8 +2,12 @@ package com.nyasama.activity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.InputType;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,7 +27,9 @@ import com.nyasama.util.Helper;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Created by oxyflour on 2015/10/18.
@@ -213,6 +219,7 @@ public class NyaSecActivity extends BaseThemedActivity {
             @Override
             public void onClick(View view) {
                 mCode.setText(getCode());
+                mGameEnabled = true;
             }
         });
 
@@ -240,5 +247,185 @@ public class NyaSecActivity extends BaseThemedActivity {
                 popup.show();
             }
         });
+
+        mGameSpan = (TextView) findViewById(R.id.nyasec_game);
+        mCountSpan = (TextView) findViewById(R.id.nyasec_count);
+        mGameSpan.setOnClickListener(mOnGameClick);
+
+        mGameTick.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isFinishing())
+                    return;
+                if (mGameEnabled)
+                    mGameSteps.step(1.0 / 4);
+                mGameTick.postDelayed(this, GAME_INTERVAL);
+            }
+        }, NyaSecActivity.GAME_INTERVAL);
+
+        try {
+            // https://www.freesound.org/people/Jaz_the_MAN_2/sounds/316901/
+            mSpanClickedSound = sound.load(getAssets().openFd("do.wav"), 0);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private final static int GAME_INTERVAL = 1000 / 16;
+    // get more at http://matsucon.net/material/dic/
+    private final static String[] GAME_SPAN_TEXTS = {
+            "ヽ(✿ﾟ▽ﾟ)ノ",
+            "(*‘ v`*)",
+            "(*´∀`)~♥",
+            "(ﾉ>ω<)ﾉ",
+            "d(`･∀･)b",
+    };
+    private final static String[] GAME_SPAN_CLKED = {
+            "(=ﾟДﾟ=)",
+            "-`д´-",
+            "(´ﾟдﾟ`)",
+    };
+
+    private TextView mGameSpan;
+    private TextView mCountSpan;
+    private boolean mGameEnabled;
+    private boolean mSpanClicked;
+    private int mSpanClickedCount;
+    private int mSpanClickedMaxCount;
+    private int mSpanClickedSound;
+    private Handler mGameTick = new Handler();
+
+    private static Random rand = new Random();
+    private static SoundPool sound = new SoundPool(1, AudioManager.USE_DEFAULT_STREAM_TYPE, 0);
+
+    // pitch = 1 => do
+    // ref: http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html
+    private static int[] MAJOR_PITCHES = { 1, 3, 5, 6, 8, 10, 12 };
+    private static float getPlayRate(int pitch) {
+        int delta = 0;
+        while (pitch > 7) {
+            pitch -= 7;
+            delta += 12;
+        }
+        while (pitch < 1) {
+            pitch += 7;
+            delta -= 12;
+        }
+        return (float) Math.pow(1.059463094359, MAJOR_PITCHES[pitch - 1] - 1 + delta);
+    }
+
+    private Runnable mOnGameUpdate = new Runnable() {
+        @Override
+        public void run() {
+            if (mSpanClicked) {
+                mSpanClickedCount ++;
+                mSpanClickedMaxCount = Math.max(mSpanClickedCount, mSpanClickedMaxCount);
+            }
+            else {
+                mSpanClickedCount = 0;
+            }
+
+            mCountSpan.setText(mSpanClickedCount > 0 ?
+                    mSpanClickedCount + " / " + mSpanClickedMaxCount : "");
+
+            mSpanClicked = false;
+            mGameSpan.setText(GAME_SPAN_TEXTS[rand.nextInt(GAME_SPAN_TEXTS.length)]);
+            mGameSpan.setVisibility(View.VISIBLE);
+
+            double fx = Math.random();
+            double fy = mGameSteps.maxVal <= mGameSteps.minVal ? 0.5 :
+                    (mGameSteps.value - mGameSteps.minVal) / (mGameSteps.maxVal - mGameSteps.minVal);
+
+            // keep it away from center
+            if (fy > 0.5)
+                fy = fy * 0.8 + 0.2;
+            else
+                fy = fy * 0.8;
+
+            DisplayMetrics metrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+            mGameSpan.setTranslationX(Math.round((0.9 - fx * 0.8) * (metrics.widthPixels - mGameSpan.getMeasuredWidth())));
+            mGameSpan.setTranslationY(Math.round((0.9 - fy * 0.8) * (metrics.heightPixels - mGameSpan.getMeasuredHeight())));
+        }
+    };
+
+    private View.OnClickListener mOnGameClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mSpanClicked = true;
+            mGameSpan.setText(GAME_SPAN_CLKED[rand.nextInt(GAME_SPAN_CLKED.length)]);
+
+            sound.play(mSpanClickedSound, 1, 1, 0, 0, getPlayRate((int)mGameSteps.value));
+
+            mGameTick.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (mSpanClicked)
+                        mGameSpan.setVisibility(View.INVISIBLE);
+                }
+            }, 100);
+        }
+    };
+
+    // Kamigami ga koishita gensoukyo
+    private GameSteps mGameSteps = new GameSteps(
+            "3,3,5,6," + "5,6,5,3,2,5,3," +
+            "3,3,5,6," + "5,6,8,7,6,5,6," +
+            "5,6,5,3,2," + "5,6,5,2,1," +
+            "6,6,7,1," + "2,7,6,6", 3, mOnGameUpdate);
+
+    class GameSteps {
+        GameSteps(String valueStr, int defDuration, Runnable onUpdate) {
+            for (String val : valueStr.split(",")) {
+                String[] vs = val.split(":");
+                double v = Helper.toSafeDouble(vs[0], 0);
+                minVal = Math.min(minVal, v);
+                maxVal = Math.max(maxVal, v);
+                values.add(v);
+
+                double d = vs.length > 1 ? Helper.toSafeDouble(vs[1], defDuration) : defDuration;
+                total += d;
+                durations.add(d);
+            }
+
+            update = onUpdate;
+        }
+
+        Runnable update;
+
+        ArrayList<Double> values = new ArrayList<Double>();
+        ArrayList<Double> durations = new ArrayList<Double>();
+        double total = 0;
+        double minVal = Double.MAX_VALUE;
+        double maxVal = Double.MIN_VALUE;
+
+        int lastIndex = -1;
+        double value = 0;
+        void updateValue(double progress) {
+            double start = 0;
+            for (int i = 0; i < durations.size(); i ++) {
+                double next = start + durations.get(i);
+                if (start <= progress && progress < next) {
+                    value = values.get(i);
+                    if (lastIndex != i)
+                        update.run();
+                    lastIndex = i;
+                    return;
+                }
+                start = next;
+            }
+        }
+
+        double progress;
+        void step(double time) {
+            progress += time;
+            if (total <= 0)
+                progress = 0;
+            else while (progress >= total)
+                progress -= total;
+            updateValue(progress);
+        }
     }
 }
